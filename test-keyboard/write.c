b2653bffedfa9877a946074acafcb220ab00f614
nino-porcino <nino.porcino@gmail.com>
2021-03-27 15:04:21 +0100

    add write keyboard test

---
+++ test-keyboard/write.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void set_port(int port) __z88dk_fastcall {
+    #asm
+    ld a,l
+    ld (portsmc+1),a
+    #endasm
+}
+
+unsigned char read_port() __z88dk_fastcall {
+    #asm
+portsmc:
+    in a,(0xf8)
+    ld l,a
+    ld h,0
+    #endasm
+}
+
+unsigned char vti_key_ascii() {
+    return read_port() & 0x7f;
+}
+
+unsigned char vti_keyboard_pressed() {
+    return (read_port() & 0x80) != 0;
+}
+
+unsigned char c;
+unsigned char c1;
+unsigned char c2;
+
+void main(void) {
+    set_port(0xe8);
+    printf("Write... press ESC to exit\r\n");
+
+    while(1) {
+        c1=read_port() & 0x80;
+        do {
+            c2=read_port() & 0x80;
+        } while(c1);
+
+        c = vti_key_ascii();
+        printf("%c",c);
+        if(c==27 || c==(27|0x80)) break;
+    }
+
+    printf("done\r\n");
+}
